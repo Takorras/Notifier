ff07b66becd89640d483c418f091459d038e3eeb
Takorras <sy.empress0321@gmail.com>
2017-07-27 15:29:38 +0900

    [create]MainActivityのデザイン

--- app/src/main/kotlin/kotako/java/info/notifyer/MainActivity.kt
+++ app/src/main/kotlin/kotako/java/info/notifyer/MainActivity.kt
@@ -2,11 +2,15 @@ package kotako.java.info.notifyer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        toolbar.title = "Notifier"
     }
 }
