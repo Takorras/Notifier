db10fec3f6948f5c6828c9a96f1c1dfc51d5d81a
Takorras <sy.empress0321@gmail.com>
2017-07-27 16:23:05 +0900

    [create]MainActivityのToolbar

--- app/src/main/kotlin/kotako/java/info/notifyer/MainActivity.kt
+++ app/src/main/kotlin/kotako/java/info/notifyer/MainActivity.kt
@@ -2,6 +2,8 @@ package kotako.java.info.notifyer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
 import android.support.v7.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +12,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val drawer = findViewById(R.id.drawer) as DrawerLayout
+
+//      setting Toolbar
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         toolbar.title = "Notifier"
+        toolbar.setNavigationIcon(R.mipmap.menu_white)
+        toolbar.setNavigationOnClickListener { drawer.openDrawer(GravityCompat.START) }
     }
 }
