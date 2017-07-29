ff0ba8eb15219410ce9af23ee364abd19babda23
Takorras <sy.empress0321@gmail.com>
2017-07-30 00:50:24 +0900

    []ReciclerViewの実装（ロジックは除く）

--- app/src/main/kotlin/kotako/java/info/notifyer/MainActivity.kt
+++ app/src/main/kotlin/kotako/java/info/notifyer/MainActivity.kt
@@ -6,7 +6,6 @@ import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.widget.Toolbar
-import android.view.MenuItem
 import android.widget.Toast
 import kotako.java.info.notifyer.Event.NavigationEvent
 import kotako.java.info.notifyer.Event.ToastEvent
@@ -29,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         toolbar.title = "Notifier"
         toolbar.setNavigationIcon(R.mipmap.menu_white)
         toolbar.setNavigationOnClickListener { drawer.openDrawer(GravityCompat.START) }
-        toolbar.inflateMenu(R.menu.toolbar_menu)
+        toolbar.inflateMenu(R.menu.menu_toolbar)
         toolbar.setOnMenuItemClickListener {
             Toast.makeText(applicationContext, "add", Toast.LENGTH_SHORT).show()
             true
@@ -38,6 +37,7 @@ class MainActivity : AppCompatActivity() {
 //      setting NavigationView
         val navigation = findViewById(R.id.navigation) as NavigationView
         navigation.setNavigationItemSelectedListener(NavigationListener())
+        navigation.setCheckedItem(R.id.recently_menu)
     }
 
     override fun onStop() {
@@ -48,12 +48,15 @@ class MainActivity : AppCompatActivity() {
     @Subscribe(threadMode = ThreadMode.MAIN)
     fun onNavigationChanged(e: NavigationEvent) {
         val drawer = findViewById(R.id.drawer) as DrawerLayout
+        val navigation = findViewById(R.id.navigation) as NavigationView
+
+        navigation.setCheckedItem(e.itemId)
         drawer.closeDrawer(GravityCompat.START)
     }
 
     @Subscribe(threadMode = ThreadMode.ASYNC)
     fun showToast(e: ToastEvent) {
-        runOnUiThread { Toast.makeText(this, e.msg, Toast.LENGTH_SHORT).show() }
+        runOnUiThread { Toast.makeText(applicationContext, e.msg, Toast.LENGTH_SHORT).show() }
     }
 }
 
--- app/src/main/kotlin/kotako/java/info/notifyer/View/TasksFragment.kt
+++ app/src/main/kotlin/kotako/java/info/notifyer/View/TasksFragment.kt
@@ -1,21 +1,47 @@
 package kotako.java.info.notifyer.View
 
-
-import android.app.ListFragment
+import android.app.Fragment
+import android.content.Context
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotako.java.info.notifyer.R
+import org.greenrobot.eventbus.EventBus
 
-class TasksFragment : ListFragment() {
+class TasksFragment : Fragment() {
+
+    var applicationContext :Context? = null
+
+    fun TasksFragment() {}
+
+    fun newInstance() {
+        return TasksFragment()
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        applicationContext = context!!.applicationContext
+        EventBus.getDefault().register(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle): View? {
-        //return inflater.inflate();
-        return container!!.rootView
+        val view = inflater.inflate(R.layout.fragment_tasks, container, false)
+        val fragment = view.findViewById(R.id.fragment_tasks) as RecyclerView
+
+        fragment.layoutManager = LinearLayoutManager(applicationContext!!)
+        return view
+    }
+
+    override fun onPause() {
+        EventBus.getDefault().unregister(this)
+        super.onPause()
     }
 
 }
