965075aeccdbb4dd2515fef19ffcac3dfce090d4
Takorras <sy.empress0321@gmail.com>
2017-07-29 18:19:08 +0900

    [create]Toolbar Add Menu, Navigation Menu

--- app/src/main/kotlin/kotako/java/info/notifyer/MainActivity.kt
+++ app/src/main/kotlin/kotako/java/info/notifyer/MainActivity.kt
@@ -2,15 +2,25 @@ package kotako.java.info.notifyer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import android.widget.Toast
+import kotako.java.info.notifyer.Event.NavigationEvent
+import kotako.java.info.notifyer.Event.ToastEvent
+import kotako.java.info.notifyer.View.NavigationListener
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        EventBus.getDefault().register(this)
 
         val drawer = findViewById(R.id.drawer) as DrawerLayout
 
@@ -19,5 +29,32 @@ class MainActivity : AppCompatActivity() {
         toolbar.title = "Notifier"
         toolbar.setNavigationIcon(R.mipmap.menu_white)
         toolbar.setNavigationOnClickListener { drawer.openDrawer(GravityCompat.START) }
+        toolbar.inflateMenu(R.menu.toolbar_menu)
+        toolbar.setOnMenuItemClickListener {
+            Toast.makeText(applicationContext, "add", Toast.LENGTH_SHORT).show()
+            true
+        }
+
+//      setting NavigationView
+        val navigation = findViewById(R.id.navigation) as NavigationView
+        navigation.setNavigationItemSelectedListener(NavigationListener())
+    }
+
+    override fun onStop() {
+        EventBus.getDefault().unregister(this)
+        super.onStop()
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onNavigationChanged(e: NavigationEvent) {
+        val drawer = findViewById(R.id.drawer) as DrawerLayout
+        drawer.closeDrawer(GravityCompat.START)
+    }
+
+    @Subscribe(threadMode = ThreadMode.ASYNC)
+    fun showToast(e: ToastEvent) {
+        runOnUiThread { Toast.makeText(this, e.msg, Toast.LENGTH_SHORT).show() }
     }
 }
+
+
---
+++ app/src/main/kotlin/kotako/java/info/notifyer/View/NavigationListener.kt
@@ -0,0 +1,13 @@
+package kotako.java.info.notifyer.View
+
+import android.support.design.widget.NavigationView
+import android.view.MenuItem
+import kotako.java.info.notifyer.Event.NavigationEvent
+import org.greenrobot.eventbus.EventBus
+
+class NavigationListener : NavigationView.OnNavigationItemSelectedListener{
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        EventBus.getDefault().post(NavigationEvent(item.itemId))
+        return false
+    }
+}
