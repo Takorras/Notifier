7447d8a47e47caffc878edc8cafb17a4930774e3
Takorras <sy.empress0321@gmail.com>
2017-09-10 19:45:19 +0900

    [fix]鎮火

---
+++ app/src/main/kotlin/info/kotako/Taaker/View/Fragment/CategoryFragment.kt
@@ -0,0 +1,50 @@
+package info.kotako.Taaker.View.Fragment
+
+import android.app.Fragment
+import android.os.Bundle
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import info.kotako.Taaker.Model.Task
+import info.kotako.Taaker.R
+import info.kotako.Taaker.View.Adapter.CategoryRecyclerViewAdapter
+import io.realm.Realm
+
+class CategoryFragment : Fragment() {
+
+    var recyclerView: RecyclerView? = null
+    var list: ArrayList<String> = arrayListOf("None")
+
+    companion object {
+        fun newInstance(): CategoryFragment {
+            return CategoryFragment()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // RealmでTaskのカテゴリ名を全て持ってくる、重複はない幹事
+        Realm.getDefaultInstance().use { realm ->
+            realm.executeTransaction {
+                list.addAll(realm.where(Task::class.java).distinct("genre").map { value -> value.genre }.toTypedArray())
+                list.remove("")
+            }
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_tasks, container, false)
+        recyclerView = view.findViewById(R.id.fragment_tasks) as RecyclerView
+        recyclerView!!.addItemDecoration(DividerItemDecoration(recyclerView!!.context, LinearLayoutManager(activity).orientation))
+        recyclerView!!.layoutManager = LinearLayoutManager(activity.applicationContext)
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        recyclerView!!.adapter = CategoryRecyclerViewAdapter(list)
+    }
+}
---
+++ app/src/main/kotlin/info/kotako/Taaker/View/Fragment/DoneTasksFragment.kt
@@ -0,0 +1,73 @@
+package info.kotako.Taaker.View.Fragment
+
+import android.app.Fragment
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.realm.Realm
+import io.realm.Sort
+import info.kotako.Taaker.Event.TaskDestroyEvent
+import info.kotako.Taaker.Model.Task
+import info.kotako.Taaker.R
+import info.kotako.Taaker.View.Adapter.TaskRecyclerViewAdapter
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
+
+class DoneTasksFragment : Fragment() {
+
+    var recyclerView: RecyclerView? = null
+    var list: ArrayList<Task> = ArrayList()
+    var realm: Realm? = null
+
+    companion object {
+        fun newInstance(): DoneTasksFragment {
+            return DoneTasksFragment()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        EventBus.getDefault().register(this)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_tasks, container, false)
+        //(view.findViewById(R.id.text_title_list) as TextView).text = "達成したタスク"
+        recyclerView = view.findViewById(R.id.fragment_tasks) as RecyclerView
+        recyclerView!!.layoutManager = LinearLayoutManager(activity.applicationContext)
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+//      RealmでTaskを全て持ってくる
+        realm = Realm.getDefaultInstance()
+        list.addAll(realm!!.where(Task::class.java).equalTo("isDone", true).findAllSorted("milestone", Sort.ASCENDING))
+
+        recyclerView!!.adapter = TaskRecyclerViewAdapter(list)
+    }
+
+    override fun onStop() {
+        EventBus.getDefault().unregister(this)
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        realm!!.close()
+        super.onDestroy()
+    }
+
+    //  realmからの削除とカードの削除
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onTaskDestroyed(e: TaskDestroyEvent) {
+        val result = realm!!.where(Task::class.java).equalTo("id", list.removeAt(e.id).id).findAllSorted("milestone", Sort.DESCENDING)
+        realm!!.executeTransaction { result.deleteAllFromRealm() }
+
+        recyclerView!!.adapter.notifyItemRemoved(e.id)
+    }
+}
